c7b9aa3866a332fa3251427bfcc25cb87023bb4c
kwonja <snna58@naver.com>
2021-12-27 19:55:41 +0900

    [PG] 타켓넘버-Lv.2

---
+++ 권성민 알고리즘 코드/타겟넘버 Level-2.cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers, int target) {
+	int answer = 0; int n = 2;
+	vector<int>tree; tree.push_back(0); tree.push_back(0);
+	for (int i = 0; i < numbers.size(); i++)
+	{
+		for (int j = n; j < (n * 2); j++)
+		{
+			if (j % 2 == 0)tree.push_back(tree[j / 2] - numbers[i]);
+			else tree.push_back(tree[j / 2] + numbers[i]);
+		}
+		n = n * 2;
+	}
+	n = n / 2;
+	for (int i = n; i < (n * 2); i++)if (tree[i] == target)answer++;
+	return answer;
+}
