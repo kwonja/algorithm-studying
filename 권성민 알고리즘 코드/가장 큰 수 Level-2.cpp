0a9febb1c7f5a6c3d60d241f1cb081bf29e17e14
kwonja <snna58@naver.com>
2021-12-28 16:45:55 +0900

    [PG]가장큰수Lv-2

---
+++ 권성민 알고리즘 코드/가장 큰 수 Level-2.cpp
@@ -0,0 +1,31 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<int> numbers) {
+	string answer = "";
+	string comp1; string comp2;
+	int temp;
+	for (int i = 0; i < numbers.size() - 1; i++)
+	{
+		for (int j = 0; j < numbers.size() - i - 1; j++)
+		{
+			comp1 = to_string(numbers[j]) + to_string(numbers[j + 1]);
+			comp2 = to_string(numbers[j + 1]) + to_string(numbers[j]);
+
+			if (stoi(comp2) > stoi(comp1))
+			{
+				temp = numbers[j];
+				numbers[j] = numbers[j + 1];
+				numbers[j + 1] = temp;
+			}
+		}
+	}
+	for (int i = 0; i < numbers.size(); i++)
+	{
+		answer += to_string(numbers[i]);
+	}
+
+	return answer;
+}
