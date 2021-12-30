8592fe557160c516dd04b3a29a29a3392aad15c2
kwonja <snna58@naver.com>
2021-12-30 23:24:34 +0900

    [BOJ]회전

---
+++ 권성민 알고리즘 코드/회전.cpp
@@ -0,0 +1,29 @@
+﻿#include <cstring>
+#include <deque>
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main(void)
+{
+	deque<char> a;
+	char arr[10];
+	long long sum = 0;
+	cin >> arr;
+
+	for (int i = 0; i < strlen(arr); i++)
+	{
+		a.push_back(arr[i]);
+	}
+	for (int i = 0; i < a.size(); i++)
+	{
+		a.push_front(a.back());
+		a.pop_back();
+		for (int j = 0; j < a.size(); j++)
+		{
+			sum = sum + (((int)a[(a.size() - 1) - j] - 48) * pow(10, j));
+		}
+	}
+	cout << sum << endl;
+	return 0;
+}
