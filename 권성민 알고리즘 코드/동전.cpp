5ab621fa5a10c3a334a0195cc5a323fa24ec68e8
kwonja <kownja87@gmail.com>
2022-01-21 12:19:52 +0900

    [BOJ] 동전

---
+++ 권성민 알고리즘 코드/동전.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int main(void)
+{
+	vector<int> arr;
+	int n, k;
+	int count = 0;
+	cin >> n >> k;
+	int input;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> input;
+		arr.push_back(input);
+	}
+	for (int i = arr.size() - 1; i >= 0; i--)
+	{
+		count += (k / arr[i]);
+		k = k % arr[i];
+		if (k == 0)break;
+	}
+	cout << count;
+	return 0;
+}
