99c388c51880feda7571ebd8681a7b4ff809996b
kwonja <kownja87@gmail.com>
2022-02-08 21:44:55 +0900

    [BOJ] 요세푸스문제

---
+++ 권성민 알고리즘 코드/요세푸스문제0.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <deque>
+using namespace std;
+int main()
+{
+	int n, k;
+	cin >> n; cin >> k;
+	deque<int> arr;
+	for (int i = 0; i < n; i++)
+	{
+		arr.push_back(i + 1);
+	}
+	cout << "<";
+	while (!arr.empty())
+	{
+		if (arr.size() == 1)break;
+		for (int i = 0; i < k - 1; i++)
+		{
+			arr.push_back(arr.front());
+			arr.pop_front();
+		}
+		cout << arr.front() << ", ";
+		arr.pop_front();
+	}
+	cout << arr.front() << ">" << endl;
+	return 0;
+}
