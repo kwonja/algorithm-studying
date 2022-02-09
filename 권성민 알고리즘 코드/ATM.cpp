844a5b2dc84aef691a0de4474f0df93f0cff8640
kwonja <kownja87@gmail.com>
2022-02-09 17:46:36 +0900

    [BOJ]ATM

---
+++ 권성민 알고리즘 코드/ATM.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main(void)
+{
+	int n;
+	int p, pi = 0;
+	vector<int> arr;
+	int sum = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> p;
+		arr.push_back(p);
+	}
+	sort(arr.begin(), arr.end()); //기본 오름차순 정렬
+	for (int i = 0; i < n; i++)
+	{
+		pi = pi + arr[i];
+		sum += pi;
+	}
+	cout << sum << endl;
+
+	return 0;
+}
