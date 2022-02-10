decb12101084a0d0f4817eeebfda300d45d3ecbe
kwonja <kownja87@gmail.com>
2022-02-10 16:50:58 +0900

    [BOJ]스택

---
+++ 권성민 알고리즘 코드/스택.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+int main()
+{
+	stack<int> arr;
+	int n, input;
+	string str;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> str;
+		if (str == "push")
+		{
+			cin >> input;
+			arr.push(input);
+			continue;
+		}
+		if (str == "pop")
+		{
+			if (arr.size() == 0)
+			{
+				cout << -1 << endl;
+				continue;
+			}
+			cout << arr.top() << endl;
+			arr.pop();
+			continue;
+		}
+		if (str == "size")
+		{
+			cout << arr.size() << endl;
+			continue;
+		}
+		if (str == "empty")
+		{
+			cout << arr.empty() << endl;
+			continue;
+		}
+		if (str == "top")
+		{
+			if (arr.size() == 0)
+			{
+				cout << -1 << endl;
+				continue;
+			}
+			cout << arr.top() << endl;
+			continue;
+		}
+	}
+	return 0;
+}
