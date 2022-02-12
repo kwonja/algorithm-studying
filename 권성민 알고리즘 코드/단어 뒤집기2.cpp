e1837fc5c4dfa52ceb52b05323d9f9d34ee84dca
kwonja <kownja87@gmail.com>
2022-02-12 16:49:58 +0900

    [BOJ]단어뒤집기2

---
+++ 권성민 알고리즘 코드/단어 뒤집기2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+int main()
+{
+	stack<char> arr;
+	int cnt = 0;
+	string str;
+	getline(cin, str);
+	for (int i = 0; i <= str.length(); i++)
+	{
+		if (cnt == 0)
+		{
+			if (str[i] == '<')
+			{
+				while (1)
+				{
+					cout << str[i];
+					if (str[i] == '>')
+					{
+						i++;
+						break;
+					}
+					i++;
+				}
+			}
+		}
+		if (str[i] == ' ' || str[i] == NULL || str[i] == '<')
+		{
+			while (!arr.empty())
+			{
+				cout << arr.top();
+				arr.pop();
+			}
+			if (str[i] == ' ')cout << " ";
+			if (str[i] == '<')i--;
+			cnt = 0;
+			continue;
+		}
+		arr.push(str[i]);
+		cnt = 1;
+	}
+	return 0;
+}
