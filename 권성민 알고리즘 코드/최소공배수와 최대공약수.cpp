d42849f320d68b69f48982792dab414f56cfdc86
kwonja <kownja87@gmail.com>
2022-02-17 17:43:36 +0900

    [BOJ] 최대공약수와 최대공배수

---
+++ 권성민 알고리즘 코드/최소공배수와 최대공약수.cpp
@@ -0,0 +1,30 @@
+﻿//유클리드 호제법으로 최대공약수,최소공배수 구하기
+#include <stdio.h>
+int func1(int a, int b)
+{
+	int c;
+	while (b)
+	{
+		c = a % b;
+		a = b;
+		b = c;
+	}
+	return a;
+}
+int main()
+{
+	int n, k;
+	scanf("%d %d", &n, &k);
+	if (n > k)
+	{
+		printf("%d\n", func1(n, k));
+		printf("%d\n", n*k / func1(n, k));
+
+	}
+	else
+	{
+		printf("%d\n", func1(k, n));
+		printf("%d\n", n*k / func1(k, n));
+	}
+	return 0;
+}
