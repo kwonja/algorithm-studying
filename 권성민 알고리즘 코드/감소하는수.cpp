052ff66e3dee37db2be422957e3d7ca70959eb86
kwonja <kownja87@gmail.com>
2022-02-20 18:22:52 +0900

    [BOJ]감소하는수

---
+++ 권성민 알고리즘 코드/감소하는수.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main()
+{
+	vector <long long> arr;
+	for (int i = 0; i < 10; i++)
+	{
+		arr.push_back(i);
+	}
+	int n;
+	cin >> n;
+	if (n > 1022)
+	{
+		cout << -1;
+		return 0;
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 0; j < (arr[i] % 10); j++)
+		{
+			arr.push_back(arr[i] * 10 + j);
+		}
+	}
+	cout << arr[n];
+	return 0;
+}
