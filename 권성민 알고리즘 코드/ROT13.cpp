c8245d8d1ebec94edb784115c006c0d36edf8def
kwonja <kownja87@gmail.com>
2022-02-21 18:29:49 +0900

    [BOJ]ROT13

---
+++ 권성민 알고리즘 코드/ROT13.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string str;
+	getline(cin, str);
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (str[i] == ' ')continue;
+		else if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] = str[i] - 13;
+			if (str[i] < 'A')
+			{
+				str[i] = str[i] + 26;
+			}
+		}
+		else if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 13;
+			if (str[i] < 'a')
+			{
+				str[i] = (str[i] + 26);
+			}
+		}
+		else if (str[i] >= '0' && str[i] <= '9')continue;
+	}
+	cout << str;
+	return 0;
+}
