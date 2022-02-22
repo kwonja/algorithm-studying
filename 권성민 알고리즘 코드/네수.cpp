4498785bdc93645fda24f1923166493021875e8a
kwonja <kownja87@gmail.com>
2022-02-22 15:38:10 +0900

    [BOJ]네수

---
+++ 권성민 알고리즘 코드/네수.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string a, b, c, d, e, f;
+	cin >> a >> b >> c >> d;
+	e = a + b;
+	f = c + d;
+	long long result = stoll(e) + stoll(f);
+	cout << result;
+	return 0;
+	//이어붙이고 나면 그 수가 엄청 커진다
+}
