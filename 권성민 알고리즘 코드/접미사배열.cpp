2ad73260da47b7799e5128ffe83de1f4c0415d16
kwonja <kownja87@gmail.com>
2022-02-22 16:23:55 +0900

    [BOJ]접미사배열

---
+++ 권성민 알고리즘 코드/접미사배열.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string str, temp;
+	string str1[1001];
+	cin >> str;
+	for (int i = 0; i < str.length(); i++)
+	{
+		for (int j = i; j < str.length(); j++)
+		{
+			str1[i] += str[j];
+		}
+	}
+	for (int i = 0; i < str.length() - 1; i++)
+	{
+		for (int j = i + 1; j < str.length(); j++)
+		{
+			if (str1[i] > str1[j])
+			{
+				temp = str1[j];
+				str1[j] = str1[i];
+				str1[i] = temp;
+			}
+		}
+	}
+	//알파벳의 사전은 크기순!!!!
+	for (int i = 0; i < str.length(); i++)
+	{
+		cout << str1[i] << endl;
+	}
+}
